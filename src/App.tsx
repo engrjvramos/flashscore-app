1c263807e82277d1e54c9a3523b2e320f4fe42cd
Jose Ramos <engrjvramos@gmail.com>
2023-05-13 00:56:15 +0800

    initialize project

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+export default function App() {
+  return (
+    <div>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Non
+      necessitatibus, iure eaque quaerat impedit dicta et sequi odio, quasi
+      repellat repudiandae? Soluta cumque recusandae quod. Veniam, molestias
+      rerum deserunt dolores autem ipsam nostrum totam aperiam dicta ex mollitia
+      vero repudiandae adipisci expedita, voluptatem voluptates illo voluptate
+      asperiores error atque ea.
+    </div>
+  );
+}
